2eb517678d987f0356f7c42c96e5c35aa020226d
Tim Johnson <tim.johnson@WDAPL011.local>
2018-11-28 16:58:19 -0500

    modify previous function using closures

--- closures.js
+++ closures.js
@@ -17,27 +17,24 @@
 
 //   }
 
-
-function interviewQuestion(job){
-  var name = Henry
-  if(job === 'designer'){
-    return function(){
+function interviewQuestion (job) {
+  return function (name) {
+    if (job === 'designer') {
       console.log(name + ', can you please explain what UX design is?')
-    }
-  } else if (job === 'teacher') {
-    return function() {
+    } else if (job === 'teacher') {
       console.log('What subject do you teach, ' + name + '?')
-    }
-  } else {
-    return function() {
+    } else {
       console.log('Hello ' + name + ', what do you do?')
     }
   }
+}
 
-  }
-  
-  var interviewQuestionDesigner =  interviewQuestion('designer')
+var interviewQuestionDesigner = interviewQuestion('designer')
 
-  var interviewQuestionTeacher =  interviewQuestion('teacher')
+var interviewQuestionTeacher = interviewQuestion('teacher')
 
-  var interviewQuestionGeneric =  interviewQuestion('generic')
+var interviewQuestionGeneric = interviewQuestion('generic')
+
+interviewQuestionDesigner('Jim')
+interviewQuestionTeacher('Hilary')
+interviewQuestionGeneric('Vincent')
