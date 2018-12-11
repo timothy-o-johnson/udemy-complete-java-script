5ece6f35c27e0d9a9b31f30c693a89b038f06ca8
Tim Johnson <tim.johnson@corp.woodlanddirect.com>
2018-12-11 17:14:36 -0500

    fix syntax, rename files

--- resources/js/challenge7.js
+++ resources/js/challenge7.js
@@ -5,47 +5,46 @@ function Question (question, choices, answer) {
     answer: answer,
     displayQuestion: function () {
       var choices = this.choices
-      
-      for(var i = 0; i < choices.length ; i++){
-        choices += i + ': '  + choices[i] + '/n'
+      var allChoices = ''
+
+      for (var i = 0; i < choices.length; i++) {
+        allChoices += i + ': ' + choices[i] + '\n'
       }
-      
-      return question + '( ' + choices + ' )'
+
+      return this.question + '\n' + allChoices
     }
   }
 }
 
 var guessMyBirthday = Question("When's my birthday?", ['July 1', 'Oct 3', 'Sep 24'], 0)
-var guessCurrentLocation = Question('When do I live these days?', ['Germany', 'Chicago', 'Detroit'], 2)
+var guessCurrentLocation = Question('Where do I live these days?', ['Germany', 'Chicago', 'Detroit'], 2)
 var guessFavoriteAuthor = Question("Who's my favorite author?", ['Sylvia Plath', 'David Foster Wallace', 'Dr Seuss'], 1)
 
 var questions = [guessMyBirthday, guessCurrentLocation, guessFavoriteAuthor]
+var playAgain = 'y'
 
 function playRound (questions) {
- // select random questions
-  var whichQuestion = Math.ceil(Math.random() * questions.length-1)
+  // select random questions
+  var whichQuestion = Math.ceil(Math.random() * questions.length - 1)
   var question = questions[whichQuestion]
 
-  var  answer = prompt(question.displayQuestion())
+  question.displayQuestion()
 
-  if(answer === question.answer){
+  var answer = window.prompt(question.displayQuestion())
+
+  if (answer === question.answer.toString()) {
     console.log('bing bong! that is correct')
   } else {
-    console.log('oops! try agin')
+    console.log('oops! try again')
   }
 
-  playAgain = prompt('Play again?').toLowerCase
-  
+  playAgain = prompt('Play again?').toLowerCase()
+
   return playAgain
 }
 
-
-var playAgain
-
-playAgain = playRound(questions)
-
-while(playAgain === 'y' || playAgain === 'yes'){
+while (playAgain === 'y' || playAgain === 'yes') {
   playRound(questions)
-} 
+}
 
 console.log("that's the end friend!")
