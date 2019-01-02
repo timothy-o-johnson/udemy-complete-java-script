13ee1f4c8caa43175c5a1b845ef341d18c5302a6
Tim Johnson <tim.johnson@wdapl011.lan>
2019-01-02 08:27:22 -0500

    [fc] initial commit

---
+++ Budget App/app.js
@@ -0,0 +1,17 @@
+
+//BUDGET CONTROLLER
+var budgetController = (function () {
+  // some code
+})()
+
+
+// UI CONTROLLER
+var UIController = (function () {
+  // some code
+})()
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+  // some code
+  document.querySelector('.add__btn')
+})(budgetController, UIController)
