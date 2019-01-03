37f96d2c094c05f45f6531215a485961724c1f63
Tim Johnson <tim.johnson@wdapl011.lan>
2019-01-03 17:18:39 -0500

    [fc] add evenlisteners; create ctrlAdddItem()

--- Budget App/app.js
+++ Budget App/app.js
@@ -1,9 +1,7 @@
-
-//BUDGET CONTROLLER
+// BUDGET CONTROLLER
 var budgetController = (function () {
   // some code
 })()
-
 
 // UI CONTROLLER
 var UIController = (function () {
@@ -13,5 +11,24 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
   // some code
-  document.querySelector('.add__btn')
+
+  var ctrlAddItem = function () {
+    // 1. Get field input data
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+
+    console.log('event', event)
+  }
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+  document.addEventListener('keypress', function (event) {
+    // if(event = )
+    if (event.keyCode === 13 || event.which === 13) {
+      console.log('and that there, my friend, is how you press enter!')
+
+      ctrlAddItem()
+    }
+  })
 })(budgetController, UIController)
