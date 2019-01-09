7be61b7649552586502487fb946f20dc0f8d8233
Tim Johnson <tim.johnson@corp.woodlanddirect.com>
2019-01-09 18:11:23 -0500

    [fc] add function and obj to pass data from UICtrl to GlobalAppCtrl

--- Budget App/app.js
+++ Budget App/app.js
@@ -5,23 +5,41 @@ var budgetController = (function () {
 
 // UI CONTROLLER
 var UIController = (function () {
-  // some code
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputAddBtn: '.add__btn'
+  }
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+    DOMstrings: DOMstrings
+  }
 })()
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-  // some code
+  var DOMstrings = UIController.DOMstrings
 
   var ctrlAddItem = function () {
     // 1. Get field input data
+    var input = UIController.getInput()
+    console.log(input)
+
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-
-    console.log('event', event)
   }
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+  document.querySelector(DOMstrings.inputAddBtn).addEventListener('click', ctrlAddItem)
 
   document.addEventListener('keypress', function (event) {
     // if(event = )
