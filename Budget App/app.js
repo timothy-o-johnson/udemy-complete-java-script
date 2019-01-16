6753ebb6cc23269394085967dd8072d9d04b17d0
timothy-o-johnson <tim.johnson@WDAPL011.local>
2019-01-16 08:40:52 -0500

    [sx] testing

--- Budget App/app.js
+++ Budget App/app.js
@@ -48,8 +48,8 @@ var controller = (function (budgetCtrl, UICtrl) {
     var input = UIController.getInput()
     console.log(input)
 
-    // 2. Add the item to the budget controller
-    // 3. Add the item to the UI
+    // 2. Add item to the budget controller
+    // 3. Add item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
   }
