2f08600dee75cd2a52b42dcb4a370194a3c5a9a2
timothy-o-johnson <pavementii@gmail.com>
2019-02-12 12:12:20 -0500

    [fc] convert field input into numbers; prevent false inputs

--- Budget App/app.js
+++ Budget App/app.js
@@ -71,7 +71,7 @@ var UIController = (function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // will be either inc (income) or exp (expense)
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       }
     },
     addListItem: function (obj, type) {
@@ -148,25 +148,32 @@ var controller = (function (budgetCtrl, UICtrl) {
     })
   }
 
+  var updateBudget = function () {
+    // 1. Calculate the budget
+    // 2. Return the budget
+    // 3. Display the budget on the UI
+  }
+
   var ctrlAddItem = function () {
     var input, newItem
     // 1. Get field input data
     input = UIController.getInput()
-    console.log(input)
 
-    // 2. Add item to the budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value)
-    budgetController.testing()
+    if ((input.description !== '') && !isNaN(input.value) && input.value > 0) {
+      // 2. Add item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value)
+      budgetController.testing()
 
-    // 3. Add item to the UI
-    UICtrl.addListItem(newItem, input.type)
+      // 3. Add item to the UI
+      UICtrl.addListItem(newItem, input.type)
 
-    // 4. Clear the fields
-    UICtrl.clearFields()
-    // 5. Calculate the budget
-    // 6. Display the budget on the UI
+      // 4. Clear the fields
+      UICtrl.clearFields()
+
+      // Calculate and update budget
+      updateBudget()
+    }
   }
-
   return {
     init: function () {
       console.log('Application has started.')
